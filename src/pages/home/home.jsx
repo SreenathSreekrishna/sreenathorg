e67976b2bc0ce2427caf9a9dedbc65a8639f98f5
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2023-08-04 16:07:45 +0530

    finish homepage and make it responsive

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,27 @@
+import './home.css';
+
+const Home = () => {
+    return <div className="home">
+        <div className="left-side">
+            <div className="text font-primary">Hi, I'm Sreenath</div>
+            <div className="nav">
+                <div className="nav-item">About Me</div>
+                <div className="nav-item">My Skills</div>
+                <div className="nav-item">My Projects</div>
+                <div className="nav-item">My Awards</div>
+            </div>
+        </div>
+
+        <div className="right-side">
+            <div className="img"></div>
+            <div className="contact">Contact Me</div>
+            <div className="socials">
+                <img src="/assets/icons/mail.png" alt="E-Mail" className="social" />
+                <img src="/assets/icons/github.png" alt="Github" className="social" />
+                <img src="/assets/icons/discord.png" alt="Discord" className="social" />
+            </div>
+        </div>
+    </div>;
+}
+
+export default Home;
