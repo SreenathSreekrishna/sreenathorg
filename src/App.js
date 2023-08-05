0bd9be8b337bb806d5a46063aae920b8939237e6
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2023-08-05 17:21:29 +0530

    make home properly responsive and finish about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './pages/home/home';
+import About from './pages/about/about';
 
 const App = () => {
   return <>
     <Home />
+    <About />
   </>;
 }
 
---
+++ src/pages/about/about.jsx
@@ -0,0 +1,17 @@
+import './about.css';
+
+const About = () => {
+    return <div className="about" id="about">
+        <div className="heading font-primary">
+            About Me
+        </div>
+        <div className="about-container">
+            <p className="about-text">
+            I'm a ninth grader from Delhi, India who loves to explore and develop new technologies. I have three years of programming experience . In my free time, I play the violin and I am learning to play the guitar.
+            </p>
+            <div className="about-img"></div>
+        </div>
+    </div>
+}
+
+export default About;
