ad9ba4f24e7aeccea2d4df03cbad84cc1191d3af
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2023-08-08 20:55:17 +0530

    finish gallery

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './pages/home/home';
 import About from './pages/about/about';
 import Skills from './pages/skills/skills';
+import Gallery from './pages/gallery/gallery';
 
 const App = () => {
   return <>
     <Home />
     <About />
     <Skills />
+    <Gallery />
   </>;
 }
 
